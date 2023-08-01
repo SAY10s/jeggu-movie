08e7b338a369a1966359cbef5ae13c89e096521a
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-08-01 16:56:22 +0200

    KURWA

--- src/Osiagniecia.js
+++ src/Osiagniecia.js
@@ -21,24 +21,19 @@ const Osiagniecia = () => {
         <br></br>
 
         <div className="big">Role główne:</div>
-        <div>Mayne - Nikodem Majzner</div>
-        <div>Henry Gracjinov - Gracjan Karpiński</div>
-        <div>Gregory Dudek - Paweł Oślizło</div>
-        <div>Hackerman - Bartosz Ratajczyk</div>
-        <div>William "Montenegro" Black - Szymon Ewertowski</div>
-        <div>William "Montenegro" Black (dubbing) - Maricn Pacin</div>
+        <div>Paweł - Nikodem Majzner</div>
+        <div>Jager - Gracjan Karpiński</div>
+        <div>Bizon - Paweł Oślizło</div>
+        <div>Conan - Mateusz Daszkiewicz</div>
 
         <br></br>
 
         <div className="big">W pozostałych rolach:</div>
-        <div>Charlie - Arkadiusz Radzikowski</div>
-        <div>Łukasz (głos) - Nikodem Majzner</div>
-        <div>Paweł Oślizło (głos) - Paweł Oślizło</div>
-        <div>Nikodem Majzner (głos) - Nikodem Majzner</div>
-        <div>Informator w barze - Bartosz Ratajczyk</div>
-        <div>Generał - Paweł Oślizło</div>
-        <div>Operator kamery w scenie barowej - Paweł Oślizło</div>
-        <div>Gangsterzy - Paweł Oślizło && Bartosz Ratajczyk</div>
+        <div>Arkadiusz Radzikowski</div>
+        <div>Szymon Ewertowski</div>
+        <div>Bartosz Ratajczyk</div>
+        <div>Michał Adamczyk</div>
+        <div>Mateusz Wach</div>
       </div>
     </>
   );
